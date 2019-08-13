37820ab3fffd16d31a56950d5eae9c1059a8de8b
omattman <matnhansen@gmail.com>
2019-08-14 01:06:20 +0200

    added frontpage

---
+++ src/pages/index.js
@@ -0,0 +1,54 @@
+import React from "react"
+import { graphql } from "gatsby"
+import { Link } from "gatsby"
+import { Helmet } from "react-helmet"
+
+import Layout from "../components/layout"
+import { colors, space, mediaQueries, fontWeights } from "../utils/presets"
+import Container from "../components/container"
+
+class IndexRoute extends React.Component {
+  render() {
+    return (
+      <Layout location={this.props.location}>
+        <Helmet>
+          <meta name="Description" content="meta desc" />
+        </Helmet>
+        <main
+          id={`reach-skip-nav`}
+          css={{
+            display: `flex`,
+            flexDirection: `row`,
+            flexWrap: `wrap`,
+            justifyContent: `space-between`,
+          }}
+        >
+          <div css={{ flex: `1 1 100%` }}>
+            <Container withSidebar={false}>
+              <section>
+                <h1 css={{ fontWeight: fontWeights[1], marginTop: 0 }}>
+                  Welcome
+                </h1>
+                <ol>
+                  <li>
+                    links
+                    <ul>
+                      <li>
+                        <Link to="/docs/">Docs</Link>
+                      </li>
+                      <li>
+                        <Link to="/solutions/">Solutions</Link>
+                      </li>
+                    </ul>
+                  </li>
+                </ol>
+              </section>
+            </Container>
+          </div>
+        </main>
+      </Layout>
+    )
+  }
+}
+
+export default IndexRoute
