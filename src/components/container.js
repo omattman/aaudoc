4536f2516f524dfed46eef06637446fb588c7386
Matthias Hansen <matnhansen@gmail.com>
2019-08-13 23:23:51 +0200

    default container for all pages

--- src/components/container.js
+++ src/components/container.js
@@ -1,14 +1,25 @@
-import system from "system-components/emotion";
+import React from "react"
 
-const Container = system(
-  {
-    is: "div",
-    px: 3,
-    mx: "auto",
-    maxWidth: 1024
-  },
-  "maxWidth"
-);
-Container.displayName = "Container";
+import { space, sizes, mediaQueries } from "../utils/presets"
 
-export default Container;
+const Container = ({ children, withSidebar = true, overrideCSS }) => (
+  <div
+    css={{
+      maxWidth: withSidebar
+        ? sizes.mainContentWidth.withSidebar
+        : sizes.mainContentWidth.default,
+      margin: `0 auto`,
+      padding: space[6],
+      position: `relative`,
+      [mediaQueries.lg]: {
+        paddingTop: space[9],
+        paddingBottom: space[9],
+      },
+      ...overrideCSS,
+    }}
+  >
+    {children}
+  </div>
+)
+
+export default Container
