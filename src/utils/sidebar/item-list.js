cd5952cdb294c31f60af112af363031f127cf598
Matthias Hansen <matnhansen@gmail.com>
2019-08-13 23:36:54 +0200

    Handling active links in sidebar

---
+++ src/utils/sidebar/item-list.js
@@ -0,0 +1,42 @@
+import docsSidebar from "../../data/doc-links.yaml"
+import solutionsSidebar from "../../data/solution-links.yaml"
+
+const createHash = link => {
+  let index = -1
+  if (link) index = link.indexOf(`#`)
+  return index >= 0 ? link.substr(index + 1) : false
+}
+
+const extendItem = (items, parentTitle, level) => {
+  items.forEach(item => {
+    item.hash = createHash(item.link)
+    item.parentTitle = parentTitle
+    item.level = level || 1
+
+    if (item.items) extendItem(item.items, item.title, item.level + 1)
+  })
+}
+
+const extendItemList = itemList => {
+  itemList.forEach(section => {
+    section.level = 0
+    if (section.items) extendItem(section.items, section.title)
+  })
+  return itemList
+}
+
+const extendSidebarData = item => {
+  return {
+    title: item[0].title,
+    breadcrumbTitle: item[0].breadcrumbTitle,
+    key: item[0].key,
+    disableExpandAll: item[0].disableExpandAll,
+    disableAccordions: item[0].disableAccordions,
+    items: extendItemList(item[0].items),
+  }
+}
+
+const itemListDocs = extendSidebarData(docsSidebar)
+const itemListSolutions = extendSidebarData(solutionsSidebar)
+
+export { itemListDocs, itemListSolutions }
