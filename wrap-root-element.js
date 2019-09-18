c3749685a301f588b726b62928204af0c96c0ac5
Matthias Hansen <matnhansen@gmail.com>
2019-09-18 22:38:57 +0200

    Added components to wrap around MDXRenderer

--- wrap-root-element.js
+++ wrap-root-element.js
@@ -1,9 +1,11 @@
 import React from "react"
 import { MDXProvider } from "@mdx-js/react"
 import GuideList from "./src/components/guide-list"
+import CodeBlock from "./src/components/code-block"
 
 const components = {
   GuideList,
+  pre: CodeBlock,
 }
 
 export default ({ element }) => (
