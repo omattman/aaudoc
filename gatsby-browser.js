84d921020e170e6ea303a559023319670a50d129
omattman <matnhansen@gmail.com>
2019-11-26 00:55:41 +0100

    display message in PWA if service worker finds an update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,13 @@
-import wrapRoot from "./wrap-root-element"
+import wrapRoot from "./wrap-root-element";
 
-export const wrapRootElement = wrapRoot
+export const wrapRootElement = wrapRoot;
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  );
+  if (answer === true) {
+    window.location.reload();
+  }
+};
