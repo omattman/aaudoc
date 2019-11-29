cbe934eee7cd1decca8c023c459e7e0554186d84
Matthias Hansen <matnhansen@gmail.com>
2019-11-29 23:51:57 +0100

    Refactor Expand to function component

--- src/components/sidebar/button-expand-all.js
+++ src/components/sidebar/button-expand-all.js
@@ -9,45 +9,45 @@ const iconStyles = {
   mr: 2
 };
 
-const ExpandAllButton = ({ onClick, expandAll }) => (
-  <button
-    onClick={onClick}
-    sx={{
-      alignItems: `center`,
-      bg: `transparent`,
-      border: `none`,
-      borderRadius: 1,
-      color: `textMuted`,
-      cursor: `pointer`,
-      display: `flex`,
-      flexGrow: 0,
-      fontSize: 0,
-      lineHeight: `solid`,
-      py: 2,
-      textAlign: `left`,
-      transition: t => `all ${t.transition.speed.fast}`,
-      "&:hover": {
-        bg: `sidebar.itemHoverBackground`,
-        color: `navigation.linkHover`
-      }
-    }}
-  >
-    {expandAll ? (
-      <React.Fragment>
-        <span sx={iconStyles}>
-          <GoFold />
-        </span>
-        <span>Collapse All</span>
-      </React.Fragment>
-    ) : (
-      <React.Fragment>
-        <span sx={iconStyles}>
-          <GoUnfold />
-        </span>
-        <span>Expand All</span>
-      </React.Fragment>
-    )}
-  </button>
-);
-
-export default ExpandAllButton;
+export default function ExpandAllButton({ onClick, expandAll }) {
+  return (
+    <button
+      onClick={onClick}
+      sx={{
+        alignItems: `center`,
+        bg: `transparent`,
+        border: `none`,
+        borderRadius: 1,
+        color: `textMuted`,
+        cursor: `pointer`,
+        display: `flex`,
+        flexGrow: 0,
+        fontSize: 0,
+        lineHeight: `solid`,
+        py: 2,
+        textAlign: `left`,
+        transition: t => `all ${t.transition.speed.fast}`,
+        "&:hover": {
+          bg: `sidebar.itemHoverBackground`,
+          color: `navigation.linkHover`
+        }
+      }}
+    >
+      {expandAll ? (
+        <React.Fragment>
+          <span sx={iconStyles}>
+            <GoFold />
+          </span>
+          <span>Collapse All</span>
+        </React.Fragment>
+      ) : (
+        <React.Fragment>
+          <span sx={iconStyles}>
+            <GoUnfold />
+          </span>
+          <span>Expand All</span>
+        </React.Fragment>
+      )}
+    </button>
+  );
+}
