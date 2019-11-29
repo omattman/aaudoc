91190e3e43dfaf9d3f67fab130f1c6fcc30ab8ac
Matthias Hansen <matnhansen@gmail.com>
2019-11-29 23:33:19 +0100

    Cleaned up copy template

--- src/components/copy.js
+++ src/components/copy.js
@@ -7,7 +7,13 @@ import copyToClipboard from "../utils/copy-to-clipboard";
 
 const delay = duration => new Promise(resolve => setTimeout(resolve, duration));
 
-function Copy({ className, content, duration, fileName, trim = false }) {
+export default function Copy({
+  className,
+  content,
+  duration,
+  fileName,
+  trim = false
+}) {
   const [copied, setCopied] = useState(false);
 
   const label = copied
@@ -67,5 +73,3 @@ Copy.defaultProps = {
   duration: 5000,
   fileName: ``
 };
-
-export default Copy;
